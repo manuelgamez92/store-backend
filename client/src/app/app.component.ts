6b08c74ccf35cf88f325febc57f21de1c25e367c
Manuel <manuelgamez92@gmail.com>
2020-09-25 16:55:26 -0700

    Initial setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/paginations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  products: IProduct[] = [];
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.http.get("https://localhost:5001/api/products").subscribe((data:IPagination)=>{
+    console.log(data);
+    this.products=data.data;
+    },error =>{
+      console.log(error);
+    });
+  }
 }
